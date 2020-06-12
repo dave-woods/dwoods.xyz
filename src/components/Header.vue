de8b9f0de80f88f5751dd47bf651547c7b5b40c5
David Woods <dwoodscs@gmail.com>
2020-06-12 22:51:20 +0100

    Slight change to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
-    <v-app-bar app>
-        <v-img src="@/assets/logo.png" max-width="64px" contain></v-img><span class="ml-2">David Woods</span>
+    <v-app-bar app absolute>
+        <v-toolbar-title>David Woods</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <router-link to="/"><v-img src="@/assets/logo.png" max-width="64px" style="margin-right: -16px" contain></v-img></router-link>
     </v-app-bar>
 </template>
 
