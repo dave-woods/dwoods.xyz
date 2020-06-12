f7dca0aebfa19720292149f53499dea5af3a4412
David Woods <dwoodscs@gmail.com>
2020-06-12 20:19:45 +0100

    Updating (tmp?) logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app-bar app>
-        David Woods
+        <v-img src="@/assets/logo.png" max-width="64px" contain></v-img><span class="ml-2">David Woods</span>
     </v-app-bar>
 </template>
 
