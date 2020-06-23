f6050485a00da4ae7394febf40d81413844a997e
David Woods <dwoodscs@gmail.com>
2020-06-23 16:09:51 +0100

    Added hero images to routes

--- src/views/Research.vue
+++ src/views/Research.vue
@@ -1,10 +1,17 @@
 <template>
-    <div>
-        <!-- TODO: Add a highlight to the noun below -->
-        <h1>I'm a <span class="who-am-i">researcher</span>.</h1>
+    <v-container>
+        <v-row>
+            <v-col>
+                <h1>I'm a <span class="who-am-i">researcher</span>.</h1>
+            </v-col>
+        </v-row>
+        <v-row>
         <!-- Add large, related image -->
-        <!-- Add full descriptive text and examples of related works/links -->
-    </div>
+            <v-col>
+                <v-img height="200px" src="@/assets/img/research-large.jpg"></v-img>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
