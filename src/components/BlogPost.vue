9d8908045e9b4ed330d060967e8c8538b191779d
David Woods <dwoodscs@gmail.com>
2020-08-06 02:37:58 +0100

    Display some metadata on blog posts

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,5 +1,13 @@
 <template>
-    <div v-html="computedMarkdown"></div>
+    <div>
+        <div v-html="computedMarkdown"></div>
+        <hr/>
+        <div>{{ postDate }}</div>
+        <div v-if="postTags && postTags.length > 0">
+            <router-link class="primary white--text pa-1 ma-2" v-for="tag in postTags" :to="`/section/${tag}`" :key="`tag-${tag}`">+ {{tag}}</router-link>
+        </div>
+        <hr/>
+    </div>
 </template>
 
 <script>
@@ -8,7 +16,9 @@ export default {
     props: ['postId'],
     data() {
         return {
-            contents: 'Loading post'
+            contents: 'Loading post',
+            postDate: '',
+            postTags: null
         }
     },
     computed: {
@@ -17,13 +27,19 @@ export default {
         }
     },
     methods: {
-        loadPost() {
-            import(`raw-loader!@/assets/posts/${this.postId}.md`)
+        async loadPost() {
+            await import(`raw-loader!@/assets/posts/${this.postId}.md`)
             .then(m => {
                 this.contents = marked(m.default)
             })
             .catch (() => {
                 this.contents = 'Error: post not found'
+            })
+            await import('@/assets/meta/posts')
+            .then(data => data.default.find(p => p.id === this.postId))
+            .then(({date, tags}) => {
+                this.postDate = date
+                this.postTags = tags
             })
         }
     },
