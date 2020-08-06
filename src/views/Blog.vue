9defc0f1c8306b9e24eaee997bdcde890e2bf6dd
David Woods <dwoodscs@gmail.com>
2020-08-06 19:05:05 +0100

    Add twitter display to blog

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -7,25 +7,43 @@
         <h1>Write Post{{ !saved ? ' *' : ''}}</h1>
         
         <v-textarea class="monospace" auto-grow placeholder="Write your markdown here" v-model="contents"></v-textarea>
+        <!-- Should maybe _.debounce the above -->
         <v-btn key="save-text-button" @click="downloadMarkdown">Save Markdown</v-btn>
         <div v-html="computedMarkdown"></div>
     </div>
     <div v-else>
-        <v-list>
-            <v-list-item v-for="bp in blogPosts" :key="bp.id">
-            <v-card>
-                <router-link :to="`/blog/read/${bp.id}`"><v-card-title>{{ bp.title }}</v-card-title></router-link>
-                <v-card-subtitle>{{ bp.date }}</v-card-subtitle>
-                <v-card-text>{{ bp.description }}</v-card-text>
-            </v-card></v-list-item>
-        </v-list>
-        <v-btn key="new-post-button" to="/blog/write">New</v-btn>
+        <v-container>
+            <v-row>
+                <v-col cols="12" sm="6">
+                    <v-card flat height="600" class="blog-card">
+                        <v-list>
+                            <v-list-item v-for="bp in blogPosts" :key="bp.id">
+                            <v-card flat tile class="flex">
+                                <router-link :to="`/blog/read/${bp.id}`"><v-card-title>{{ bp.title }}</v-card-title></router-link>
+                                <v-card-subtitle>{{ bp.date }}</v-card-subtitle>
+                                <v-card-text>{{ bp.description }}</v-card-text>
+                            </v-card></v-list-item>
+                        </v-list>
+                        <v-btn key="new-post-button" to="/blog/write">New</v-btn>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" sm="6">
+                    <twitter-feed url="dwoodscs"  height="600"></twitter-feed>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
 
 <style scoped>
 .monospace {
     font-family: monospace;
+}
+.blog-card {
+    overflow-y: auto
+}
+.blog-card::-webkit-scrollbar {
+    display: none;
 }
 </style>
 
@@ -34,10 +52,12 @@ import marked from 'marked'
 import DOMPurify from 'dompurify'
 import list from '@/assets/meta/posts'
 import BlogPost from '@/components/BlogPost'
+import TwitterFeed from '@/components/TwitterFeed.vue'
 export default {
     props: ['postId', 'writing'],
     components: {
-        'blog-post': BlogPost
+        'blog-post': BlogPost,
+        'twitter-feed': TwitterFeed
     },
     data() {
         return {
