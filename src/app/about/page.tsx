f2135c040a5c0d4291de9a97b8a142d6ee89313f
dave-woods <11319140+dave-woods@users.noreply.github.com>
2025-10-01 17:54:03 +0100

    Start expanding about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,12 @@
+import { merriweather } from "@/utils/fonts"
+
 export default function About() {
     return (
         <main>
             <h1>About Me</h1>
+            <div className={merriweather.className}>
             <p>
-                { "Hi, I'm David Woods. I'm a software developer passionate about building web applications with modern technologies like TypeScript and React." }
+                { "Hi, I'm David Woods. I like to think of myself as the kind of person who's constantly trying to learn and grow, then taking that new knowledge and building things with it, and teaching others the tricks that helped me along the way. My path has taken me through all kinds of strange places (like France), and has taught me the only sure way to move forwards is to start by making yourself uncomfortable." }
             </p>
             <p>
                 { "I enjoy solving complex problems, learning new frameworks, and collaborating with others to create impactful digital experiences." }
@@ -11,6 +14,7 @@ export default function About() {
             <p>
                 { "Outside of coding, I love gymnastic trampolining, worldbuilding, and teaching." }
             </p>
+            </div>
         </main>
     );
 }
