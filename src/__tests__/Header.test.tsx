f356cc57d82b6472d94c62a28acf1135542ed73f
dave-woods <11319140+dave-woods@users.noreply.github.com>
2025-10-01 16:04:45 +0100

    Update test for header links

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -11,17 +11,20 @@ describe('Header', () => {
     expect(nav).toBeInTheDocument()
   })
 
-  it('renders a link to the homepage', () => {
+  it('renders the correct links', () => {
     render(<Header />)
-    const link = screen.getByRole('link', { name: 'David Woods' })
-    expect(link).toBeInTheDocument()
-    expect(link).toHaveAttribute('href', '/')
-  })
-
-    it('renders a link to the about page', () => {
-    render(<Header />)
-    const link = screen.getByRole('link', { name: 'About' })
-    expect(link).toBeInTheDocument()
-    expect(link).toHaveAttribute('href', '/about')
+    const links = [
+      { name: 'David Woods', href: '/' },
+      { name: 'About', href: '/about' },
+      { name: 'Projects', href: '/projects' },
+      { name: 'Teaching', href: '/teaching' },
+      { name: 'Creative', href: '/creative' },
+      { name: 'Contact', href: '/contact' },
+    ]
+    links.forEach(({ name, href }) => {
+      const link = screen.getByRole('link', { name })
+      expect(link).toBeInTheDocument()
+      expect(link).toHaveAttribute('href', href)
+    })
   })
 })
