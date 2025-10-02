2f0008ecc6ad586f588778a4bd94eabdc9a7cddd
dave-woods <11319140+dave-woods@users.noreply.github.com>
2025-10-02 18:10:43 +0100

    Add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import styles from './Button.module.css';
+
+export default function Button({href, level, children, onClick}: Readonly<{href?: string, level: 1 | 2 | 3, children: React.ReactNode, onClick?: () => void}>) {
+    return href ? (
+        <Link href={href} className={`${styles.button} ${styles[level === 1 ? 'primary' : level === 2 ? 'secondary' : 'tertiary']}`} onClick={onClick}>{children}</Link>
+    ) : (
+        <button className={`${styles.button} ${styles[level === 1 ? 'primary' : level === 2 ? 'secondary' : 'tertiary']}`} onClick={onClick}>{children}</button>
+    );
+}
