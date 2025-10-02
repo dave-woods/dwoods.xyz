86c3626b99374bd92174f739f46b6a222a5a78f4
dave-woods <11319140+dave-woods@users.noreply.github.com>
2025-10-02 18:14:34 +0100

    Update global styles, adding tester cards and buttons to home and projects pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import { merriweather } from "@/utils/fonts";
 import styles from "./homepage.module.css";
+import Card from "@/components/Card";
+import Link from "next/link";
+import Button from "@/components/Button";
 
 export default function Home() {
   return (
@@ -13,6 +16,26 @@ export default function Home() {
       <p className={`${styles.description} ${merriweather.className}`}>
         { "I love to make things: code 💻, gymnasts 🤸, and fictional worlds 🗺️. Welcome to my digital space." }
       </p>
+      <div className={styles.grid}>
+        <Card>
+          <h3>Latest Project: This site!</h3>
+          <p>This is a complete rewrite of my personal website for 2025. It's built using NextJS (React), CI/CD with Github and Vercel, and Jest for testing.</p>
+          <p>Feel free to check out my other recent projects <Link href="/projects">here</Link></p>
+        </Card>
+        <Card className="highlight">
+          <h3>This card has a button</h3>
+          <div className={styles.buttonRow}>
+            <Button level={3}>Click Me</Button>
+          </div>
+        </Card>
+        <Card>
+          <h3>This card has 2 buttons!</h3>
+          <div className={styles.buttonRow}>
+            <Button level={1}>Primary</Button>
+            <Button href="/projects" level={2}>Secondary</Button>
+          </div>
+        </Card>
+      </div>
     </main>
   );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,23 @@
+'use client';
+
+import Button from "@/components/Button";
+import Card from "@/components/Card";
+
 export default function Projects() {
     return (
         <main>
             <h1>Projects</h1>
             <p>This is the projects page.</p>
+            <Card>
+                <h3>Project Card Example</h3>
+                <p>This is an example of a project card.</p>
+                <Button href="/projects/example" level={2}>Learn More</Button>
+            </Card>
+            <Card>
+                <h3>Project Card Example</h3>
+                <p>This button alerts the page.</p>
+                <Button onClick={() => alert('Alert!')} level={3}>Alert?</Button>
+            </Card>
         </main>
     );
 }
